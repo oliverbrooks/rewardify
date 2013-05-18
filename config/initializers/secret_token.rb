f70b917ed68e3fbc0ce0a158dd11487026871c6c
Oliver Brooks <oli@valobox.com>
2013-05-18 15:39:55 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rewardify::Application.config.secret_token = 'd74845aaaa76ee5d5a3c9afe1f4475f8b6a29202008b2b450d0d4bca5ab516a26ee5deece0db7ff1eedaa0f04d793f06ad66e3294fb043435bcc9405f5d5d1b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rewardify::Application.config.session_store :cookie_store, key: '_rewardify_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rewardify::Application.config.session_store :active_record_store
