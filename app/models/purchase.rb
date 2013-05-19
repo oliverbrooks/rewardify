14f0988e70c0d07ff1b40952cd89af4f7975a78e
Oliver Brooks <oli@valobox.com>
2013-05-19 10:08:12 +0100

    add sms messaging

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,9 +1,9 @@
 class Purchase < ActiveRecord::Base
-  attr_accessible :notes, :user_id, :value, :vendor_id
-
   belongs_to :user
   belongs_to :vendor
   belongs_to :offer
+
+  scope :pending, -> { where(played: false) }
 
   validate :user, :vendor, :value, :access_token, presence: true
 
@@ -14,4 +14,17 @@ class Purchase < ActiveRecord::Base
   def generate_access_token
     self.access_token = SecureRandom.hex[0..10]
   end
+
+  def send_offer_message
+    TWILIO_CLIENT.account.sms.messages.create(from: ENV['TWILIO_NUMBER'], to: phone, body: play_game_message)
+  end
+
+  def play_game_message
+    "You've been awarded #{value} points. Click this link for a chance to double your points #{play_game_link}"
+  end
+
+  def play_game_link
+    Rails.application.routes.url_helpers.play_url(t: access_token)
+  end
+
 end
