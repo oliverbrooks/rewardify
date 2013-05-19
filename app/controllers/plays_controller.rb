9754e4407920426f638a42cf456bbffba12824ec
Oliver Brooks <oli@valobox.com>
2013-05-19 14:10:39 +0100

    add play layout

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,24 +1,27 @@
 class PlaysController < ApplicationController
 
-  before_filter :set_access_token
+  layout 'play'
 
   def edit
+    set_access_token
     @user = @purchase.user
     sign_in @user
   end
 
   def update
+    set_access_token
     @purchase.update_attribute(:played, true)
     redirect_to played_path(t: @purchase.access_token)
   end
 
   def show
+    set_access_token(true)
   end
 
   private
 
-    def set_access_token
-      @purchase = Purchase.where(played: false, access_token: params[:t]).first
+    def set_access_token(played = false)
+      @purchase = Purchase.where(played: played, access_token: params[:t]).first
 
       unless @purchase.present?
         redirect_to root_url, notice: "Sorry, we couldn't find that prize code"
